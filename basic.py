23f9a131140d64a4e65095626ea1ed7a8ac3007b
nonen <nonenow99@gmail.com>
2021-12-16 18:12:40 +0900

    21.12.16

--- basic.py
+++ basic.py
@@ -1,1 +1,9 @@
 # Python 기초
+print("코드잇" + "유재석")
+print("3" + "5")
+print("Hello" * 3)
+
+# 문자열 안에 따옴표가 포함되는 경우
+print('I'm excited to learn Python!')  # Syntax 오류
+print("I'm excited to learn Python!")  # 큰따옴표로 바꿈
+print('I\'m excited to learn Python!')  # 역슬래시(\) 이용
