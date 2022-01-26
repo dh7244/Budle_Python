44619bcba2f6bde5cf2462e850c73aa10fdf5b8a
nonen <nonenow99@gmail.com>
2022-01-27 00:18:26 +0900

    22.1.26 알고리즘 3강 진행중

--- basic.py
+++ basic.py
@@ -1,15 +1,5 @@
 # 알고리즘
 
-def fib_tab(n):
-    fib_list = [0, 1, 1]
-    i = 3
-    while i <= n:
-        fib_list.append(fib_list[i-1] + fib_list[i-2])
-        i += 1
-    return fib_list[n]
 
 
-# 테스트
-print(fib_tab(10))
-print(fib_tab(56))
-print(fib_tab(132))
+
